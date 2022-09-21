ac7df18cfff2c394fe803121f07832ef084ec5bd
Temirlan Shalqarov <Temirlan200312@gmail.com>
2022-09-21 22:52:29 +0600

    added: http client and json parser

--- index.php
+++ index.php
@@ -9,7 +9,29 @@
 </head>
 
 <body>
-    <?php echo "<h1>Hello World!</h1>"  ?>
+    <?php
+
+    require __DIR__ . '/vendor/autoload.php';
+
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Exception\RequestException;
+
+    $client = new Client([
+        'curl' => array(CURLOPT_SSL_VERIFYPEER => false,),
+    ]);
+    try {
+        $response = $client->get('https://api.thecatapi.com/v1/images/search');
+    } catch (RequestException $e) {
+        echo $e->getRequest() . "\n";
+        if ($e->hasResponse()) {
+            echo $e->getResponse() . "\n";
+        }
+    }
+    $body = $response->getBody();
+    $data = json_decode($body);
+    // print_r($data[0]->url);
+    ?>
+    <img src="<?php echo $data[0]->url ?>" />
 </body>
 
 </html>
